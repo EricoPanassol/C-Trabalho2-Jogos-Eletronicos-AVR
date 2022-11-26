1369d9e2af14e2267cce5334761a113c2dac46f7
EricoPanassol <ericopanassol@gmail.com>
2022-11-26 11:54:47 -0300

    [Fix] keypad method

--- main.c
+++ main.c
@@ -8,7 +8,6 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-
 void nokia_lcd_init(void);
 void nokia_lcd_clear(void);
 void nokia_lcd_set_cursor(uint8_t x, uint8_t y);
@@ -20,6 +19,7 @@ void nokia_lcd_drawcircle(uint8_t x1, uint8_t y1, uint8_t r);
 void startMenu();
 void resetEnemy();
 void drawElements();
+char scan_keypad();
 
 // flor
 uint8_t floor_x1 = 0;
@@ -28,7 +28,7 @@ uint8_t floor_x2 = 83;
 uint8_t floor_y2 = 43;
 
 // player
-uint8_t player_lb = 7;  
+uint8_t player_lb = 7;
 uint8_t player_tb = 33; // cresce pra 0
 uint8_t player_rb = 17;
 uint8_t player_bb = 43; // chão do eixo y
@@ -36,31 +36,22 @@ uint8_t player_bb = 43; // chão do eixo y
 // enemy
 uint8_t enemy_lb = 76;
 uint8_t enemy_tb = 33; // cresce pra 0
-uint8_t enemy_rb = 80; 
+uint8_t enemy_rb = 80;
 uint8_t enemy_bb = 43; // chão do eixo y
 
+// global variables
 uint8_t points = 0;
-uint8_t jogando = 1;
+uint8_t jogando = 0;
 uint8_t pressed = 0;
-
+uint8_t pulo = 0;
 uint8_t start = 0;
+uint16_t key;
 
 ISR(INT0_vect)
 {
     if (!(PIND & (1 << PD2)))
-    {   
+    {
         pressed = 1;
-    }
-}
-
-ISR(PCINT1_vect)
-{
-    if ((PINC & (1 << PC1)) || (PINC & (1 << PC2)))
-    {   
-        start = 1;
-        // nokia_lcd_clear();
-        // nokia_lcd_write_string("Oi", 1);
-        // nokia_lcd_render();
     }
 }
 
@@ -68,94 +59,107 @@ int main()
 {
     nokia_lcd_init();
     nokia_lcd_clear();
-    
+
     // Definindo as saídas
     DDRB |= (1 << PB0) | (1 << PB1) | (1 << PB2) | (1 << PB3) | (1 << PB4);
-    DDRC |= (1 << PC1) | (1 << PC2);
+    // configura PD6 e PD7 como saídas
+    DDRD |= (1 << PD6) | (1 << PD7);
 
     // Definindo as entradas
     DDRD &= (0 << PD2);
-    DDRC &= (0 << PC0);
-
-
-    // Definindo as interrupções
-    PCICR |= (1 << PCIE2); // habilita vetor de interrupção de todos os PDs
-
+    // configura PD5 como entrada (com pull-down externo)
+    DDRD &= ~(0 << PD5);
 
     // Habilitando o pull-up (sempre passa 1)
     PORTD |= (1 << PD2);
 
-    EICRA = 0b00000010; // interrupcao externa INT0 na borda de descida
-    EIMSK = 0b00000001; // habilita a interrupcao externa INT0
+    // Definindo as interrupções
+    PCICR |= (1 << PCIE2); // habilita vetor de interrupção de todos os PDs
+    EICRA = 0b00000010;    // interrupcao externa INT0 na borda de descida
+    EIMSK = 0b00000001;    // habilita a interrupcao externa INT0
 
     sei();
-    
-    int pulo = 0;
 
-    while(1){
+    // menu
+    while (1)
+    {
         startMenu();
-        if(start == 1){
+        key = scan_keypad();
+        if (key)
+        {
+            jogando = 1;
             break;
         }
+        break;
     }
 
     nokia_lcd_clear();
 
+    // game loop
+    while (jogando == 1)
+    {
 
-    while(jogando == 1){
-        
-        if(pressed == 1){
+        if (pressed == 1)
+        {
             pulo = 1;
 
-            if(jogando == 0){
+            if (jogando == 0)
+            {
                 return -1;
             }
             pressed = 0;
         }
 
         // subida
-        if(pulo == 1){
-            if(player_tb > 13){
+        if (pulo == 1)
+        {
+            if (player_tb > 13)
+            {
                 player_tb -= 1;
                 player_bb -= 1;
             }
-            else{
+            else
+            {
                 pulo = 0;
             }
-        // descida
-        }else{
-            if(player_bb < 43){
+            // descida
+        }
+        else
+        {
+            if (player_bb < 43)
+            {
                 player_tb += 1;
                 player_bb += 1;
             }
         }
-        
-         _delay_ms(1);
+
+        _delay_ms(1);
         resetEnemy();
         drawElements();
-        
-        if(enemy_lb <= player_rb && enemy_tb < player_bb && enemy_bb > player_tb){
+
+        if (enemy_lb <= player_rb && enemy_tb < player_bb && enemy_bb > player_tb)
+        {
             nokia_lcd_clear();
             nokia_lcd_write_string("Voce perdeu\001", 1);
             nokia_lcd_render();
             jogando = 0;
         }
     }
-
 }
 
-
-void startMenu(){
-    nokia_lcd_set_cursor(29, 0); 
+void startMenu()
+{
+    nokia_lcd_set_cursor(29, 0);
     nokia_lcd_write_string("Game", 1);
-    nokia_lcd_set_cursor(10, 17); 
+    nokia_lcd_set_cursor(10, 17);
     nokia_lcd_write_string("(1) - Start", 1);
     nokia_lcd_set_cursor(10, 34);
     nokia_lcd_write_string("(2) - Exit", 1);
     nokia_lcd_render();
 }
 
-void drawElements(){
+void drawElements()
+{
     nokia_lcd_clear();
     nokia_lcd_drawline(floor_x1, floor_y1, floor_x2, floor_y2);
     nokia_lcd_drawrect(player_lb, player_tb, player_rb, player_bb);
@@ -166,12 +170,48 @@ void drawElements(){
     nokia_lcd_render();
 }
 
-void resetEnemy(){
+void resetEnemy()
+{
     enemy_lb -= 1;
     enemy_rb -= 1;
-    if(enemy_lb <= 0 || enemy_rb <= 0){
+    if (enemy_lb <= 0 || enemy_rb <= 0)
+    {
         enemy_lb = 80;
         enemy_rb = 76;
         points++;
     }
+}
+
+// entrada - PD5
+// saida - PD6, PD7
+char scan_keypad()
+{
+    char keys[1][2] = {{'1', '2'}};
+    uint8_t i, j;
+
+    // seleciona a coluna
+    for (j = 0; j < 2; j++)
+    {
+        if (j == 0)
+        {
+            PORTD |= (1 << PD6);
+        }
+        if (j == 1)
+        {
+            PORTD |= (1 << PD7);
+        }
+        // seleciona a linha PD5 = 5
+        for (i = 5; i < 6; i++)
+        {
+            if (PIND & (1 << i))
+            {
+                // Esperamos a tecla ser solta, para evitar repetição de entrada
+                _delay_ms(5);
+            }
+        }
+        PORTD &= ~((1 << PD6) | (1 << PD7));
+        return keys[i][j];
+    }
+
+    return 0;
 }
